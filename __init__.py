6fdfaf81c0be9278eadf1aaf1ccdb5cbe283401f
Douglas La Rocca <larocca@abiresearch.com>
2015-05-28 12:23:20 -0400

    fix pkg blank inits

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .algorithms import lib
-from .algorithms import gray_code
-from .algorithms import searching
-from .algorithms import gcc_builtin
-from .algorithms import numerical
-from .algorithms.utils import libc
--- algorithms/numerical/__init__.py
+++ algorithms/numerical/__init__.py
@@ -1,2 +1,1 @@
-"""numerical
-"""
+from . import bits
